6704d3290c1bbebf78663a1d855e25bccb1c52c3
abhishek singh <abhishek.singh>
2025-03-26 12:30:12 +0530

    Explanation: Checks divisibility by 2 to determine even or odd. and swaping numbers

---
+++ Check-Even-or-Odd.py
@@ -0,0 +1,8 @@
+# Explanation: Checks divisibility by 2 to determine even or odd.
+
+number = int(input("enter the number: "))
+
+if (number % 2 == 0):
+    print(number, "is the even number")
+else:
+    print(number, "is the odd number")
---
+++ Swap-Two-Variables.py
@@ -0,0 +1,4 @@
+# Explanation: Swaps values without using a temporary variable.
+a, b = 5, 3
+a, b = b, a
+print(a, b)
