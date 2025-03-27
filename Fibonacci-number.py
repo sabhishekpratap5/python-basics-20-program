6bd35875cdce694fc97007ec765d76967c3a965e
abhishek singh <abhishek.singh>
2025-03-27 11:11:12 +0530

    Fibonacci number

---
+++ Fibonacci-number.py
@@ -0,0 +1,12 @@
+# Fibonacci Sequence
+
+def fibonacci(num):
+    if num == 0: 
+        return 0
+    elif num == 1:
+        return 1
+    # Recursive case
+    else :
+        return fibonacci(num-1) + fibonacci(num-2)
+    
+print(fibonacci(10))
